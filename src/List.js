32b36fe7b0269ac648227f5471de4995f498d370
tuanngoc1601 <tuanngoc12340@gmail.com>
2022-02-07 17:06:45 +0700

    birthday_list

---
+++ src/List.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const List = ({people}) => {
+  return (
+    <>
+        {people.map(person => (
+            <article key={person.id} className="person">
+                <img src={person.image} alt={person.name} />
+                <div>
+                    <h4>{person.name}</h4>
+                    <p>{person.age} years</p>
+                </div>
+            </article>
+        ))}
+    </>
+  );
+};
+
+export default List;
